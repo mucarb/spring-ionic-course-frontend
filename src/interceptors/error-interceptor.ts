b18115f3f377bad05c68677064ac6ce90a7a5493
mucarb <murilo.rb60@gmail.com>
2022-08-30 16:04:10 -0300

    Default handling for other errors with Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,12 +1,15 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { AlertController } from "ionic-angular";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
 
 @Injectable()
 export class ErrorInteceptor implements HttpInterceptor {
 
-  constructor(public storage: StorageService) { }
+  constructor(
+    public storage: StorageService,
+    public alertCtrl: AlertController) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req)
@@ -24,16 +27,50 @@ export class ErrorInteceptor implements HttpInterceptor {
         console.log(errorObj);
 
         switch (errorObj.status) {
+          case 401:
+            this.handle401();
+            break;
           case 403:
             this.handle403();
+            break;
+          default:
+            this.handleDefaultError(errorObj);
             break;
         }
         return Observable.throw(errorObj);
       }) as any;
   }
 
+  handle401() {
+    let alert = this.alertCtrl.create({
+      title: 'Erro 401: Falha de autenticação',
+      message: 'Email ou senha incorrentos',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'Ok',
+        }
+      ],
+    });
+    alert.present();
+  }
+
   handle403() {
     this.storage.setLocalUser(null);
+  }
+
+  handleDefaultError(errorObj) {
+    let alert = this.alertCtrl.create({
+      title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+      message: errorObj.message,
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'Ok',
+        }
+      ],
+    });
+    alert.present();
   }
 
 }
