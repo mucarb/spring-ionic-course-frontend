6807fd456eac8030adc3b6a43eb5e0d7e3b03d79
mucarb <murilo.rb60@gmail.com>
2022-09-01 17:11:03 -0300

    Ending cart features

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CartItem } from '../../models/cart-item';
+import { ProductDTO } from '../../models/product.dto';
 import { CartService } from '../../services/domain/cart.service';
 
 @IonicPage()
@@ -21,6 +22,26 @@ export class CartPage {
   ionViewDidLoad() {
     let cart = this.cartService.getCart();
     this.cartItems = cart.items;
+  }
+
+  removeItem(product: ProductDTO) {
+    this.cartItems = this.cartService.removeProduct(product).items;
+  }
+
+  increaseQuantity(product: ProductDTO) {
+    this.cartItems = this.cartService.increaseQuantity(product).items;
+  }
+
+  decreaseQuantity(product: ProductDTO) {
+    this.cartItems = this.cartService.decreaseQuantity(product).items;
+  }
+
+  total(): number {
+    return this.cartService.total();
+  }
+
+  goOn() {
+    this.navCtrl.setRoot('CategoryPage');
   }
 
 }
